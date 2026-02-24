f2f01d9f42abeef7faa69ad43d0fcb29cd55faa7
dasha_uwu <dasha@linuxping.win>
2026-02-24 14:31:43 +0500

    Drop blurhashing

--- src/core/config/mod.rs
+++ src/core/config/mod.rs
@@ -56,8 +56,8 @@ use crate::{
 ### For more information, see:
 ### https://tuwunel.chat/configuration.html
 "#,
-	ignore = "catchall well_known tls blurhashing allow_invalid_tls_certificates ldap jwt \
-	          appservice identity_provider"
+	ignore = "catchall well_known tls allow_invalid_tls_certificates ldap jwt appservice \
+	          identity_provider"
 )]
 pub struct Config {
 	/// The server_name is the pretty name of this server. It is used as a
@@ -2280,10 +2280,6 @@ pub struct Config {
 
 	// external structure; separate section
 	#[serde(default)]
-	pub blurhashing: BlurhashConfig,
-
-	// external structure; separate section
-	#[serde(default)]
 	pub ldap: LdapConfig,
 
 	// external structure; separate section
@@ -2396,34 +2392,6 @@ pub struct WellKnownConfig {
 	/// default: []
 	#[serde(default)]
 	pub rtc_transports: Vec<serde_json::Value>,
-}
-
-#[derive(Clone, Copy, Debug, Deserialize, Default)]
-#[expect(rustdoc::broken_intra_doc_links, rustdoc::bare_urls)]
-#[config_example_generator(
-	filename = "tuwunel-example.toml",
-	section = "global.blurhashing"
-)]
-pub struct BlurhashConfig {
-	/// blurhashing x component, 4 is recommended by https://blurha.sh/
-	///
-	/// default: 4
-	#[serde(default = "default_blurhash_x_component")]
-	pub components_x: u32,
-	/// blurhashing y component, 3 is recommended by https://blurha.sh/
-	///
-	/// default: 3
-	#[serde(default = "default_blurhash_y_component")]
-	pub components_y: u32,
-	/// Max raw size that the server will blurhash, this is the size of the
-	/// image after converting it to raw data, it should be higher than the
-	/// upload limit but not too high. The higher it is the higher the
-	/// potential load will be for clients requesting blurhashes. The default
-	/// is 33.55MB. Setting it to 0 disables blurhashing.
-	///
-	/// default: 33554432
-	#[serde(default = "default_blurhash_max_raw_size")]
-	pub blurhash_max_raw_size: u64,
 }
 
 #[derive(Clone, Debug, Default, Deserialize)]
@@ -3464,14 +3432,6 @@ fn default_client_response_timeout() -> u64 { 120 }
 fn default_client_shutdown_timeout() -> u64 { 15 }
 
 fn default_sender_shutdown_timeout() -> u64 { 5 }
-
-// blurhashing defaults recommended by https://blurha.sh/
-// 2^25
-fn default_blurhash_max_raw_size() -> u64 { 33_554_432 }
-
-fn default_blurhash_x_component() -> u32 { 4 }
-
-fn default_blurhash_y_component() -> u32 { 3 }
 
 fn default_ldap_search_filter() -> String { "(objectClass=*)".to_owned() }
 
